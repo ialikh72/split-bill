32751e546e6f8b139c4ca84bf92017df72be8382
ialikh72 <ialikh.w1@gmail.com>
2026-02-20 21:57:30 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the Bill:");
+        double bill = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter total persons:");
+        int persons = Convert.ToInt32(Console.ReadLine());
+        double total =bill/ persons;
+        Console.WriteLine("bill on each person is: "+total);
+    }
+}
